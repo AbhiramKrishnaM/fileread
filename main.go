4e2f9105834400b6721597eb99219abcd23c3c2d
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2022-12-07 12:09:08 +0530

    feat: small changes

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+
+	var filename string
+
+	for _, arg := range os.Args[1:] {
+		filename = arg
+	}
+
+	fmt.Println(filename)
+	/*
+		Check if the file exists
+	*/
 
 }
