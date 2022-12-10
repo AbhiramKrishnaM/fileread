b746681bd62d451064d2f5c6aa87516f3843791d
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2022-12-10 12:43:49 +0530

    feat: added file open and file read helper functions

--- main.go
+++ main.go
@@ -13,9 +13,42 @@ func main() {
 		filename = arg
 	}
 
-	fmt.Println(filename)
 	/*
 		Check if the file exists
+		if file does not exist 
+			create the file ask for the extension
+		else 
+			open the file read the contents	
 	*/
 
+if _, err := os.Stat(filename); err == nil {
+	
+fmt.Println("File exists")
+
+}else{
+	var isSuccess = createFile()
+
+
+	if isSuccess {
+fmt.Println("FIle created successfulle")
+	}
 }
+
+
+}
+
+
+
+func createFile() bool {
+	/*
+		Ask user for the name of the file to be created 
+			create the file 
+				if file creation failed return false 
+				else return true
+	*/
+
+	return true 
+}
+
+
+func openFile(){}
