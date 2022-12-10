6824a3df723f3aad8484e1d738f0f4e6fc0b86bb
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2022-12-10 12:48:12 +0530

    feat: formatting added and added open file docs

--- main.go
+++ main.go
@@ -15,40 +15,42 @@ func main() {
 
 	/*
 		Check if the file exists
-		if file does not exist 
+		if file does not exist
 			create the file ask for the extension
-		else 
-			open the file read the contents	
+		else
+			open the file read the contents
 	*/
 
-if _, err := os.Stat(filename); err == nil {
-	
-fmt.Println("File exists")
+	if _, err := os.Stat(filename); err == nil {
 
-}else{
-	var isSuccess = createFile()
+		fmt.Println("File exists")
 
+	} else {
+		var isSuccess = createFile()
 
-	if isSuccess {
-fmt.Println("FIle created successfulle")
+		if isSuccess {
+			fmt.Println("FIle created successfulle")
+		}
 	}
-}
-
 
 }
-
-
 
 func createFile() bool {
 	/*
-		Ask user for the name of the file to be created 
-			create the file 
-				if file creation failed return false 
+		Ask user for the name of the file to be created
+			create the file
+				if file creation failed return false
 				else return true
 	*/
 
-	return true 
+	return true
 }
 
-
-func openFile(){}
+func openFile() bool {
+	/*
+		Open the file
+			read the contents of the file
+			and close the file
+	*/
+	return true
+}
