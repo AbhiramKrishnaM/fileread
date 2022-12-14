5779fa173631046916cc3e045a596d15ab64ef00
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2022-12-14 10:15:27 +0530

    feat: added helper functions

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 )
 
 func main() {
@@ -58,81 +57,6 @@ func selectOption(_flag string, _arg map[int]string) bool {
 	}
 
 	return false
-}
-
-func createFile(file_name string) bool {
-
-	/*
-		create the file with existing file name
-			ask user for the file extension
-				if file creation failed return false
-				else return true
-	*/
-	var ext string
-	var foldername string
-
-	extension := map[string]string{
-		"go":   "go",
-		"js":   "js",
-		"exe":  "exe",
-		"txt":  "txt",
-		"php":  "php",
-		"jsx":  "jsx",
-		"py":   "py",
-		"yml":  "yml",
-		"json": "json",
-		"c":    "c",
-		"psd":  "psd",
-		"cpp":  "cpp",
-	}
-
-	for {
-		fmt.Println("Extension for your new file ? \t")
-		fmt.Scanf("%s", &ext)
-
-		if _, ok := extension[ext]; ok {
-			break
-		} else {
-			fmt.Printf("File extension not recognized. Please re enter \n")
-		}
-	}
-
-	currentDirectory, err := os.Getwd()
-
-	if err != nil {
-		log.Fatal(err)
-		return false
-	} else {
-		_filename := file_name + "." + ext
-
-		for {
-
-			fmt.Printf("Enter folder name. This will the folder name to which the file will be saved! \n")
-			fmt.Scanf("%s", &foldername)
-
-			absolutePath := currentDirectory + "/" + foldername
-
-			_, err := os.Create(filepath.Join(absolutePath, filepath.Base(_filename)))
-
-			if err == nil {
-				break
-			} else {
-				if err := os.Mkdir(foldername, os.ModePerm); err != nil {
-					log.Fatal(err)
-				} else {
-					_, err := os.Create(filepath.Join(absolutePath, filepath.Base(_filename)))
-
-					if err != nil {
-						break
-					}
-				}
-				break
-			}
-		}
-
-		return true
-	}
-
 }
 
 func openFile(file_name string) bool {
--- util/actions.go
+++ util/actions.go
@@ -1,8 +1,10 @@
 package util
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func ReadFile() {}
@@ -24,5 +26,87 @@ func DeleteFile(filename string) bool {
 }
 
 func DeleteFolder(foldername string) bool {
+	return true
+}
+
+/*
+Create file or folder
+*/
+
+func CreateFile(file_name string) bool {
+	/*
+		create the file with existing file name
+			ask user for the file extension
+				if file creation failed return false
+				else return true
+	*/
+	var ext string
+	var foldername string
+
+	extension := map[string]string{
+		"go":   "go",
+		"js":   "js",
+		"exe":  "exe",
+		"txt":  "txt",
+		"php":  "php",
+		"jsx":  "jsx",
+		"py":   "py",
+		"yml":  "yml",
+		"json": "json",
+		"c":    "c",
+		"psd":  "psd",
+		"cpp":  "cpp",
+	}
+
+	for {
+		fmt.Println("Extension for your new file ? \t")
+		fmt.Scanf("%s", &ext)
+
+		if _, ok := extension[ext]; ok {
+			break
+		} else {
+			fmt.Printf("File extension not recognized. Please re enter \n")
+		}
+	}
+
+	currentDirectory, err := os.Getwd()
+
+	if err != nil {
+		log.Fatal(err)
+		return false
+	} else {
+		_filename := file_name + "." + ext
+
+		for {
+
+			fmt.Printf("Enter folder name. This will the folder name to which the file will be saved! \n")
+			fmt.Scanf("%s", &foldername)
+
+			absolutePath := currentDirectory + "/" + foldername
+
+			_, err := os.Create(filepath.Join(absolutePath, filepath.Base(_filename)))
+
+			if err == nil {
+				break
+			} else {
+				if err := os.Mkdir(foldername, os.ModePerm); err != nil {
+					log.Fatal(err)
+				} else {
+					_, err := os.Create(filepath.Join(absolutePath, filepath.Base(_filename)))
+
+					if err != nil {
+						break
+					}
+				}
+				break
+			}
+		}
+
+		return true
+	}
+
+}
+
+func CreateFolder() bool {
 	return true
 }
