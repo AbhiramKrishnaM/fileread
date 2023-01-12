fab72928611d99c55bc0326407f767ca9c5fc300
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2023-01-12 22:09:20 +0530

    feat: reverted back

--- main.go
+++ main.go
@@ -1,30 +1,64 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"os"
+
+	"github.com/AbhiramKrishnaM/fileread/util"
 )
 
 func main() {
 
-	// var filename string
-
-	// for _, arg := range os.Args[1:] {
-	// 	filename = arg
-	// }
-
 	/*
-		Check if the file exists
+		check flag
+		-cf :- create a file
+		-cF :- create a folder
+		-rf :- read a file
+		-rF :- read contents of folder
+		-rI :- read contents of an image
+		-df :- delete file
+		-dF :- delete Folder
+		-r :- rename folder and file
 	*/
 
-	cwd, err := os.Getwd()
+	arguments := make(map[int]string)
 
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
+	for index, arg := range os.Args[1:] {
+		arguments[index] = arg
 	}
 
-	fmt.Println(cwd)
+	flag := arguments[0]
 
+	selectOption(flag, arguments)
+
+}
+
+func selectOption(_flag string, _arg map[int]string) bool {
+
+	switch _flag {
+	/*
+		create a file
+	*/
+	case "-cf":
+		util.CreateFile()
+
+	case "-cF":
+		util.CreateFolder()
+
+	case "-df":
+		util.DeleteFile()
+
+	case "-dF":
+		util.DeleteFolder()
+
+	case "-rf":
+		util.ReadFile()
+
+	case "-rF":
+		util.ReadFolder()
+
+	case "-rI":
+		util.ReadImage()
+	}
+
+	return false
 }
